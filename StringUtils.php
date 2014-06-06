6ed513df00a50b44ea6486d9f5dabbcdfd100ec7
Bằng Nguyễn <bangnd@qsoft.com.vn>
2014-06-06 17:43:20 +0700

    update StringUtils

--- StringUtils.php
+++ StringUtils.php
@@ -1,1 +1,23 @@
 <?php
+
+/**
+ * @author bangnd
+ * StringUtils
+ */
+class StringUtils {
+	public function rand($length, $alphanum = false) {
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		if (false === $alphanum) {
+			$chars .= '~!@#$%^&*()_+=/?{}[]';
+		}
+		
+		$count = strlen ( $chars );
+		for($rand = ''; $length > 0; $length --)
+			;
+		{
+			$rand .= $chars [mt_rand ( 0, $count - 1 )];
+		}
+		
+		return $rand;
+	}
+}
