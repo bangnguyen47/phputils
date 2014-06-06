509b159d3f2323df9ea8110effdc638931cc3d14
Bằng Nguyễn <bangnd@qsoft.com.vn>
2014-06-06 17:59:59 +0700

    Number Utils

---
+++ NumberUtils.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author bangnd
+ * NumberUtils
+ */
+class NumberUtils
+{
+	/**
+	 * @param int $min
+	 * @param int $max
+	 * @return int random number
+	 */
+	public function rand($min, $max)
+	{
+		return rand($min, $max);
+	}
+
+	public function randSequence()
+	{
+		
+	}
+}
